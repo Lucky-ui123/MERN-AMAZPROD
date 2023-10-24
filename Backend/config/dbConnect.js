693b3b22d8ecce6011867d83be4194ca84473675
Lucky <lngshlucky@email.com>
2023-10-24 20:24:58 +0530

    db update

--- Backend/config/dbConnect.js
+++ Backend/config/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL, {
+    await mongoose.connect(process.env.MONGODB_WEB_URL || process.env.MONGODB_URL , {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
